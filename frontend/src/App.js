72155272f76b45a5c87086472975daf21f35d48d
Artsiom Kunitsyn <kunitsyn-a-v@yandex.ru>
2022-12-28 01:42:52 +0100

    actions added

---
+++ frontend/src/App.js
@@ -0,0 +1,33 @@
+import React, {useState, useEffect} from 'react';
+import './App.css';
+
+const BASE_URL = 'http://localhost:8000/'
+
+function App() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    fetch(BASE_URL + 'post/all').then(
+      response => {
+        const js = response.json();
+        console.log(js);
+        if (response.ok) {
+          return js;
+        }
+        throw response
+      }).then(data => {
+      setPosts(data)
+    })
+    .catch(error => {
+      console.log(error);
+      alert(error)
+    })
+  }, [])
+  
+
+  return (
+    'hello'
+  );
+}
+
+export default App;
